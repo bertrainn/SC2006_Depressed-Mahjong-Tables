302e40c4cfe055e2c7f578b2e6f64597aec99c47
Han Zhiguang <145914897justin@gmail.com>
2022-10-11 11:13:14 +0800

    update functionalities of reportpage

--- mahjong_movers/lib/report.dart
+++ mahjong_movers/lib/report.dart
@@ -12,6 +12,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart' as UrlLauncher;
+import 'package:flutter_phone_direct_caller/flutter_phone_direct_caller.dart';
 
 class ReportPage extends StatefulWidget {
   const ReportPage({Key? key}) : super(key: key);
@@ -26,188 +28,245 @@ class _ReportPageState extends State<ReportPage> {
     false,
   ];
 
+  final String phoneNumber = "911";
+  final TextEditingController _t1 = TextEditingController();
+  final TextEditingController _controller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  String email = "";
+  bool emailValid = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        iconTheme: IconThemeData(color: const Color.fromARGB(255, 33, 126, 50)),
-        backgroundColor: const Color.fromARGB(255, 33, 126, 50),
-        elevation: 2.0,
-        centerTitle: true,
-        title: Text(
-          "Report",
-          style: TextStyle(
-              color: Color.fromARGB(255, 235, 240, 236),
-              fontWeight: FontWeight.bold),
-        ),
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          color: Color.fromARGB(255, 235, 240, 236),
-          onPressed: () {
-            Navigator.of(context).popUntil((route) => route.isFirst);
-            Navigator.popAndPushNamed(context, '/home');
-          },
-        ),
-      ),
-      body: Container(
-          padding: EdgeInsets.all(16.0),
-          child:
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              "Please select the type of the report",
-              style: TextStyle(
-                color: Color.fromARGB(255, 69, 66, 66),
-                fontSize: 17,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            SizedBox(height: 25.0),
-            GestureDetector(
-              child: buildCheckItem(
-                  title: "Verbal Abuse", isSelected: isTypeSelected[0]),
-              onTap: () {
-                setState(() {
-                  isTypeSelected[0] = !isTypeSelected[0];
-                });
-              },
-            ),
-            GestureDetector(
-              child: buildCheckItem(
-                  title: "Work-related accident",
-                  isSelected: isTypeSelected[1]),
-              onTap: () {
-                setState(() {
-                  isTypeSelected[1] = !isTypeSelected[1];
-                });
-              },
-            ),
-            GestureDetector(
-              child: buildCheckItem(
-                  title: "Other issues", isSelected: isTypeSelected[2]),
-              onTap: () {
-                setState(() {
-                  isTypeSelected[2] = !isTypeSelected[2];
-                });
-              },
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            buildReportForm(),
-            SizedBox(height: 20.0),
-            Container(
-              padding: const EdgeInsets.only(left: 100),
-              child: Material(
-                color: Colors.white,
-                borderRadius: const BorderRadius.all(Radius.circular(30.0)),
-                elevation: 5.0,
-                child: MaterialButton(
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                            title: const Text("Submit!"),
-                            content: const Text("Successful Sumbit!"),
-                            actions: [
-                              TextButton(
-                                child: const Text("Ok"),
-                                onPressed: () {
-                                  Navigator.of(context)
-                                      .popUntil((route) => route.isFirst);
-                                  Navigator.popAndPushNamed(context, '/home');
-                                },
-                              )
-                            ],
-                          );
-                        });
-                  },
-                  minWidth: 200.0,
-                  height: 42.0,
-                  child: const Text(
-                    'Submit',
-                  ),
-                ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(270, 250, 20, 20),
-              child: ElevatedButton(
-                  child: Text('Emergency \n       Call'),
-                  onPressed: () => launch("tel: 911"),
-                  style: ElevatedButton.styleFrom(
-                      backgroundColor: Color.fromARGB(255, 209, 59, 59))),
-            ),
-          ])),
-    );
-  }
-
-  buildReportForm() {
-    return Container(
-      height: 200,
-      child: Stack(
-        children: [
-          TextField(
-            maxLines: 10,
-            decoration: InputDecoration(
-              hintText: "Please briefly describe the issue",
-              hintStyle: TextStyle(
-                fontSize: 13.0,
-                color: Color(0xFFC5C5C5),
-              ),
-              border: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: Color(0xFFE5E5E5),
-                ),
-              ),
-            ),
+        appBar: AppBar(
+          iconTheme:
+              IconThemeData(color: const Color.fromARGB(255, 33, 126, 50)),
+          backgroundColor: const Color.fromARGB(255, 33, 126, 50),
+          elevation: 2.0,
+          centerTitle: true,
+          title: Text(
+            "Report",
+            style: TextStyle(
+                color: Color.fromARGB(255, 235, 240, 236),
+                fontWeight: FontWeight.bold),
           ),
-          Align(
-            alignment: Alignment.bottomCenter,
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border(
-                  top: BorderSide(
-                    width: 1.0,
-                    color: Color(0xFFA6A6A6),
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            color: Color.fromARGB(255, 235, 240, 236),
+            onPressed: () {
+              Navigator.of(context).popUntil((route) => route.isFirst);
+              Navigator.popAndPushNamed(context, '/home');
+            },
+          ),
+        ),
+        body: Form(
+            key: _formKey,
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              SizedBox(
+                height: 25.0,
+              ),
+              Text(
+                "  Please select the type of the report",
+                style: TextStyle(
+                  color: Color.fromARGB(255, 69, 66, 66),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 15.0),
+              GestureDetector(
+                child: buildCheckItem(
+                    title: "Verbal Abuse", isSelected: isTypeSelected[0]),
+                onTap: () {
+                  setState(() {
+                    isTypeSelected[0] = !isTypeSelected[0];
+                  });
+                },
+              ),
+              GestureDetector(
+                child: buildCheckItem(
+                    title: "Work-related accident",
+                    isSelected: isTypeSelected[1]),
+                onTap: () {
+                  setState(() {
+                    isTypeSelected[1] = !isTypeSelected[1];
+                  });
+                },
+              ),
+              GestureDetector(
+                child: buildCheckItem(
+                    title: "Other issues", isSelected: isTypeSelected[2]),
+                onTap: () {
+                  setState(() {
+                    isTypeSelected[2] = !isTypeSelected[2];
+                  });
+                },
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                onChanged: (val) {
+                  if (val.length > 0 &&
+                      RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(val)) email = val;
+                },
+                controller: _t1,
+                keyboardType: TextInputType.multiline,
+                textInputAction: TextInputAction.done,
+                validator: (String? text) {
+                  if (text == null || text.isEmpty) {
+                    return "Enter a value";
+                  }
+                  if (!RegExp(r'\S+@\S+\.\S+').hasMatch(email)) {
+                    return "Enter valid email";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  fillColor: Color(0xffe6e6e6),
+                  filled: true,
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                  hintText: 'Enter Your Email',
+                  hintStyle: TextStyle(
+                      color: Colors.blueGrey,
+                      fontFamily: 'RobotoSlab',
+                      fontSize: 14),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 33, 126, 50)),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 33, 126, 50)),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(12),
+                    ),
+                    borderSide:
+                        BorderSide(color: Color.fromARGB(255, 33, 126, 50)),
                   ),
                 ),
               ),
-              padding: EdgeInsets.all(8.0),
-              child: Row(
-                children: [
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Color(0xFFE5E5E5),
-                      borderRadius: BorderRadius.circular(5.0),
+              SizedBox(
+                height: 20.0,
+              ),
+              TextFormField(
+                  maxLines: 6,
+                  maxLength: 500,
+                  controller: _controller,
+                  keyboardType: TextInputType.multiline,
+                  decoration: InputDecoration(
+                    hintText: " Please briefly describe the issue",
+                    hintStyle: TextStyle(
+                      fontSize: 13.2,
+                      color: Colors.blueGrey,
                     ),
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.add,
-                        color: Color(0xFFA5A5A5),
+                    border: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Color.fromARGB(255, 33, 126, 50),
                       ),
                     ),
+                    filled: true,
                   ),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text(
-                    "Upload screenshot (optional)",
-                    style: TextStyle(
-                      color: Color(0xFFC5C5C5),
-                    ),
-                  ),
-                ],
+                  textInputAction: TextInputAction.done,
+                  validator: (String? text) {
+                    if (text == null || text.isEmpty) {
+                      return "Enter a value";
+                    }
+                    return null;
+                  }),
+              SizedBox(
+                height: 20.0,
               ),
-            ),
-          )
-        ],
-      ),
-    );
+              Container(
+                padding: const EdgeInsets.only(left: 120),
+                child: Material(
+                  color: Colors.white,
+                  borderRadius: const BorderRadius.all(Radius.circular(30.0)),
+                  elevation: 5.0,
+                  child: MaterialButton(
+                      minWidth: 200.0,
+                      height: 42.0,
+                      child: const Text(
+                        'Submit',
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState?.validate() == true) {
+                          showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: const Text("Submit!"),
+                                  content: const Text("Successful Sumbit!"),
+                                  actions: [
+                                    TextButton(
+                                        child: const Text("Ok"),
+                                        onPressed: () async {
+                                          if (_formKey.currentState!
+                                              .validate()) {
+                                            // We will use this var to show the result
+                                            // of this operation to the user
+                                            String message;
+                                            try {
+                                              // Get a reference to the `feedback` collection
+                                              final collection =
+                                                  FirebaseFirestore.instance
+                                                      .collection('reports');
+                                              await collection.doc().set({
+                                                'timestamp': FieldValue
+                                                    .serverTimestamp(),
+                                                'report': _controller.text,
+                                                'email': email,
+                                              });
+                                              message =
+                                                  'Report sent successfully';
+                                            } catch (e) {
+                                              message =
+                                                  'Error when sending report';
+                                            }
+
+                                            Navigator.of(context).popUntil(
+                                                (route) => route.isFirst);
+                                            Navigator.popAndPushNamed(
+                                                context, '/home');
+                                          }
+                                        }),
+                                  ],
+                                );
+                              });
+                        }
+                        ;
+                      }),
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.fromLTRB(240, 212, 20, 20),
+                child: ElevatedButton.icon(
+                    onPressed: _callNumber,
+                    label: Text('Emergency \n       Call'),
+                    icon: Icon(Icons.call),
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Color.fromARGB(255, 209, 59, 59))),
+              ),
+            ])));
+  }
+
+  _callNumber() async {
+    var url = Uri.parse("Tel:911");
+    if (await canLaunchUrl(url)) {
+      await launchUrl(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 
   Widget buildCheckItem({required String title, required bool isSelected}) {
